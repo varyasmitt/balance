a5136840c71fcc2e70d17c2195b6935e9f0d383e
varyasmitt <varyasmitt@icloud.com>
2022-02-07 14:53:17 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 200;
-        int replenishment = 1100;
-        int bonus = replenishment / 100;
-        boolean summ = replenishment > 1000;
-        int finalBalance = summ ? balance + replenishment + bonus : balance + replenishment;
+        int replenishment = 1000;
+        int bonus;
         if (replenishment > 1000) {
-            System.out.println(" Баланс " + finalBalance + " Бонус " + bonus);
+            bonus = replenishment / 100;
         } else {
-            System.out.println(" Баланс " + finalBalance + " Бонус 0");
+            bonus = 0;
         }
+        int finalBalance = balance + replenishment + bonus;
+        System.out.println(" Баланс " + finalBalance + " Бонус " + bonus);
     }
 }
